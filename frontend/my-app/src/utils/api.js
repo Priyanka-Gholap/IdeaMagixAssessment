377c2b3cb9b00402d89cd99e1e52ce93bac59f58
Priyanka Gholap <your-github-priyankagholap182@gmail.com>
2026-01-06 11:30:10 +0530

    commit

--- frontend/my-app/src/utils/api.js
+++ frontend/my-app/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://backend-1-pxkt.onrender.com/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://ideamagixassessment.onrender.com';
 const api = axios.create({
   baseURL: API_URL,
   headers: {
